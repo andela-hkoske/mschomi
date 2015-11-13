2c64be187c76190ceb92cc8a0f8b944d6c1f1f21
andela-cganga <chris.ganga@andela.com>
2015-11-13 14:48:12 +0300

    added basic route config file

---
+++ public/js/config.js
@@ -0,0 +1,9 @@
+(function () {
+	'use strict';
+
+	angular
+		.module('configs', ['ui.router'])
+		.config('$stateProvider', '$urlRouterProvider' function ($stateProvider, $urlRouterProvider) {
+			
+		}]);
+})();
