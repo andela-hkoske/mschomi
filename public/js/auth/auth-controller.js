6b1dcbb7f6503c92a9675027680b2675b9950d1f
gangachris <ganga.chris@outlook.com>
2015-11-13 13:30:52 +0300

    added login and logout scope functions

--- public/js/auth/auth-controller.js
+++ public/js/auth/auth-controller.js
@@ -3,7 +3,15 @@
 
 	angular
 		.module('auth', [])
-		.controller('authController', [function () {
-			
+		.controller('authController', ['$scope', function ($scope) {
+			$scope.user = {}; //get from auth
+
+			$scope.login = function (user) {
+				//connect to service
+			};
+
+			$scope.logout = function () {
+				//destroy session
+			}
 		}]);
 })();
