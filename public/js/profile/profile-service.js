dcdba63bc25eba76165ec84e2798f523103b0e20
andela-cganga <chris.ganga@andela.com>
2015-11-13 15:51:53 +0300

    scafolding for profile service

--- public/js/profile/profile-service.js
+++ public/js/profile/profile-service.js
@@ -5,12 +5,42 @@
 		.module('profileSvc', [])
 		.controller('profileService', ['$http', '$q', function ($http, $q) {
 			
+			function getMyProfile () {
+				//get my profile
+			}	
+
+			function saveMyProfile () {
+				// save profile changes
+				// during registration, we specify whether we are child or sponsor
+			}
+
+			function deleteMyProfile () {
+				//soft delete a profile
+			}
+
+			function getProfiles () {
+				//get all profiles - will rank
+			}
+
 			function getProfile (id) {
 				//get a certain user
 			}
 
+			function upvote (id) {
+				//upvote a profile
+			}
+
+			function contribute (user, receiver, amount) {
+				//make a donation
+			}
+
+			function sendMessage (user, receiver, message) {
+				//send message to user	
+			}
+
 			return {
-				getProfile: getProfile
+				getProfile: getProfile,
+				contribute: contribute
 			}
 		}]);
 })();
