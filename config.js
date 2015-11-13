94743e2db453b7615885426c74b602638f9cefa2
hannahkoske <hannah.koske@andela.com>
2015-11-13 12:33:37 +0300

    Added config vars

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  'database': 'mongodb://root:*ABCabc12#@ds037234.mongolab.com:37234/kogalo',
+  // 'database': 'mongodb://localhost/dms',
+  'port': process.env.PORT || 3000,
+  'secretKey': 'H46Blh8[01JQM,3<4C]dfO)mT9yKMa'
+};
